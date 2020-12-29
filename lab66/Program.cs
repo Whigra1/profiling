d24e5043c22e0604b4d05cc8b679d1dad757662f
Balatsenko Oleksandr <oleksandr.balatsenko@inbase.com.ua>
2020-12-29 10:18:54 +0200

    'init'

---
+++ lab66/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace lab66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shapes shapes = new Shapes();
+            Console.WriteLine("\n Inside main()\n");
+            string typeOfShape = Console.ReadLine();
+	
+            int i = 0;
+
+            for(;i<0xffffff;i++);
+            switch(typeOfShape){
+                case "square":
+                    Shapes.DrawSquare();
+                    break;
+                case "circle":
+                    shapes.DrawCircle();
+                    break;
+            }
+        }
+    }
+}
+
+class Shapes
+{
+    public void DrawCircle()
+    {
+        for(int i = 0; i < 1000000000; i++)
+        {
+            //process of drawing
+            Thread.Sleep(100);
+        }
+
+        return;
+    }
+
+    public static void DrawSquare()
+    {
+        for(int i = 0; i < 1000000000; i++)
+        {
+            //process of drawing
+            Thread.Sleep(100);
+        }
+    }
+}
